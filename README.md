a6f57edc44f5be560a161b4c9070145625753d01
Kevin Ng <kevin.ng.ngk3@gmail.com>
2025-04-28 22:49:15 -0400

    Revert to Next.JS with Shade UI

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # portfolio_website
+
+Portfolio Website utilizing the following:
+
+- Tanstack Start
+- ShadeCN UI Library
--- app/client.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { hydrateRoot } from "react-dom/client";
-import { StartClient } from "@tanstack/react-start";
-import { createRouter } from "./router";
-
-const router = createRouter();
-
-hydrateRoot(document, <StartClient router={router} />);
