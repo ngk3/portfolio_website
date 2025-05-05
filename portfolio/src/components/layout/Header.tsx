21f08b61e357d888da33e7e5d13935e2c3df4a5f
Kevin Ng <kevin.ng.ngk3@gmail.com>
2025-05-04 22:38:17 -0400

    Updated Header with some additional items

--- portfolio/src/components/layout/Header.tsx
+++ portfolio/src/components/layout/Header.tsx
@@ -1,18 +1,37 @@
+import Link from "next/link";
 
 export function Header() {
-   return (
+  const navLinks = [
+    { name: "Home", href: "/" },
+    { name: "About", href: "/about" },
+    { name: "Skills", href: "/skills" },
+    { name: "Experience", href: "/experience" },
+    { name: "Projects", href: "/projects" },
+    { name: "Contact", href: "/contact" },
+  ];
+
+  return (
     <header>
-        <div className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between bg-white p-4 shadow-md">
-            <div className="text-xl font-bold">Kevin Ng</div>
-            <nav>
-            <ul className="flex space-x-4">
-                <li><a href="#about">About</a></li>
-                <li><a href="#skills">Skills</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-            </nav>
+      <div className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between bg-white p-4 shadow-md">
+        {/* Portfolio Home */}
+        <div className="text-xl font-bold">{"Kevin Ng's Portfolio Website"}</div>
+
+        {/* Desktop Navigation */}
+        <nav className="hidden md:flex space-x-4">
+          {navLinks.map((link) => (
+            <Link key={link.name} href={link.href}>
+              {link.name}
+            </Link>
+          ))}
+        </nav>
+
+        {/* Mobile Navigation */}
+        <div className="md:hidden">
+          <Link key="Home" href="/">
+            {"Home"}
+          </Link>
         </div>
+      </div>
     </header>
-   ) 
+  );
 }
